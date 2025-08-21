57366ca6dddcb8bd24130e80dccf4d6082cd2b99
Sharjeel <sharjeel6392@gmail.com>
2025-08-21 21:32:00 +0530

    project structure ready

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # capstone_project
 Production-Ready MLOps Pipeline: From Training to Observability: This repository showcases a robust, end-to-end MLOps pipeline designed for real-world machine learning applications. It automates the entire lifecycle of an ML model, from data ingestion and model training to deployment including model retraining and continuous monitoring.
+
+
+Added cookiecutter generated project structure.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project showcases a robust, end-to-end MLOps pipeline designed for real-world machine learning applications. It automates the entire lifecycle of an ML model, from data ingestion and model training to deployment and continuous monitoring.',
+    author='Sharjeel Ansari',
+    license='',
+)
